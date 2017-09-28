82846f9ac62b456fdd8c2a8cc1a43e97deeea9a8
Philipp Hagemeister <phihag@phihag.de>
2017-09-28 06:24:16 +0200

    move sources out of main configuration

--- src/bbt.js
+++ src/bbt.js
@@ -7,13 +7,13 @@ const sources = require('./sources');
 const routes = require('./routes');
 
 
-function run_server(cfg) {
+function run_server(cfg, source_configs) {
 	const server = http.createServer();
 	const app = express();
 
 	app.cfg = cfg;
 	app.root_path = cfg('root_path');
-	app.state_handlers = sources.init(cfg, app);
+	app.state_handlers = sources.init(cfg, source_configs, app);
 
 	routes.setup(cfg, app);
 
@@ -24,11 +24,12 @@ function run_server(cfg) {
 function main() {
 	async.waterfall([
 		config.load,
-	], (err, cfg) => {
+		(cfg, cb) => sources.load_configs((err, source_configs) => cb(err, cfg, source_configs)),
+	], (err, cfg, source_configs) => {
 		if (err) {
 			throw err;
 		}
-		run_server(cfg);
+		run_server(cfg, source_configs);
 	});
 }
 
--- src/sources.js
+++ src/sources.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 const StateHandler = require('./state_handler').StateHandler;
 
 const TYPES = {
@@ -7,8 +10,7 @@ const TYPES = {
 	csde: require('./csde'),
 };
 
-function init(cfg, app) {
-	const source_configs = cfg('sources');
+function init(cfg, source_configs, app) {
 	const shs = [];
 	for (const src of source_configs) {
 		if (src.disabled) {
@@ -27,6 +29,28 @@ function init(cfg, app) {
 	return shs;
 }
 
+function load_configs(callback) {
+	const fn = path.join(path.dirname(__dirname), 'sources.json');
+	fs.readFile(fn, (err, config_json) => {
+		if (err && (err.code === 'ENOENT')) {
+			const default_fn = path.join(path.dirname(__dirname), 'default_sources.json');
+			fs.readFile(default_fn, (err, config_json) => {
+				if (err) return callback(err);
+
+				const source_configs = JSON.parse(config_json);
+				return callback(null, source_configs);
+			});
+			return;
+		}
+
+		if (err) return callback(err);
+
+		const source_configs = JSON.parse(config_json);
+		return callback(null, source_configs);
+	});
+}
+
 module.exports = {
 	init,
+	load_configs,
 };
