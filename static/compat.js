8ee4c9ce54aa4af36b9b98e725d7738c1725bd9d
Philipp Hagemeister <phihag@phihag.de>
2017-09-30 20:12:57 +0200

    commit compat

---
+++ static/compat.js
@@ -0,0 +1,16 @@
+'use strict';
+
+if (! Array.prototype.includes) {
+	Array.prototype.includes = function(el) {
+		for (var i = 0;i < this.length;i++) {
+			if (this[i] === el) return true;
+		}
+		return false;
+	}
+}
+
+if (! String.prototype.includes) {
+	String.prototype.includes = function(substr) {
+		return this.indexOf(substr) !== -1;
+	}
+}
