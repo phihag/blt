b7149f3f2a3aaa567d6bbebcd2450e0805dac62c
Philipp Hagemeister <phihag@phihag.de>
2020-05-10 17:20:37 +0200

    bupdate: prevent if .git exists
    This may happen if we mount the bup directory here.

--- src/bup_handlers.js
+++ src/bup_handlers.js
@@ -35,6 +35,14 @@ async function bupdate() {
 		if (e.code !== 'ENOENT') throw e;
 	}
 
+	const git_dir = path.join(FINAL_DIR, '.git');
+	if (await promise_utils.exists(git_dir)) {
+		throw new Error(
+			`Refusing to bupdate; ${git_dir} exists.` +
+			' Remove it to allow bupdate.');
+	}
+
+
 	const tmp_token = process.pid + '_' + Date.now() + '_' + crypto.randomBytes(4).readUInt32LE(0);
 	const tmp_dir = path.join(TMP_ROOT, 'bupdate_tmp_' + tmp_token);
 	const new_dir = path.join(tmp_dir, 'new');
