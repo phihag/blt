83d7c60e7637f2e47803aa3295210b230124e8d5
Philipp Hagemeister <phihag@phihag.de>
2020-10-03 08:43:38 +0200

    btde: Add a test covering Regionalliga West

--- test/test_btde.js
+++ test/test_btde.js
@@ -8,10 +8,14 @@ describe('btde', () => {
 	it('basic parsing', () => {
 		const str = '0~3~4~TV Refrath~1.BC Beuel|1~0~~HGHGG~G~Magee, Sam/Völker, Jan Colin~Briggs, Peter/Hess, Daniel~11~8~11~6~8~8~11~4~11~11|2~0~~HHH00~H~Nelte, Carla/Magee, Chloe~Pohl, Hannah/Kaminski, Lisa~13~11~11~~~11~3~3~~|3~0~~GHGHH~H~Schwenger, Max/Beck, Raphael~Weißkirchen, Max/Resch, Lukas~7~11~8~11~11~11~5~11~9~5|4~0~~GGHHG~G~Magee, Joshua~Goh, Giap Chin~6~7~11~11~3~11~11~9~6~11|5~0~~HHH00~H~Sandorhazi, Vivien~Kaminski, Lisa~11~11~11~~~5~1~8~~|6~2~~HGHGG~G~Nelte, Carla/Magee, Sam~Pohl, Hannah/Briggs, Peter~11~6~12~9~9~7~11~10~11~11|7~1~~GHGG0~G~Beck, Raphael~Weißkirchen, Max~13~11~6~3~~15~7~11~11~|1553292611';
 
-		assert.deepStrictEqual(btde._parse(str), {
+		assert.deepStrictEqual(btde._parse({league_key: '1BL-2020'}, str), {
 			team_names: ['TV Refrath', '1.BC Beuel'],
 			mscore: [3, 4],
 			scoring: '5x11_15^90',
+			courts: [
+				{label: '1', match_id: 'HE2'},
+				{label: '2', match_id: 'GD'}
+			],
 			matches: [{
 				name: 'HD1',
 				players: [[
@@ -83,8 +87,12 @@ describe('btde', () => {
 	it('parse empty match', () => {
 		const str = '0~0~0~TV Refrath 2~BC Hohenlimburg|1~0~~00000~~~~~~~~~~~~~|2~0~~00000~~~~~~~~~~~~~|3~0~~00000~~~~~~~~~~~~~|4~0~~00000~~~~~~~~~~~~~|5~0~~00000~~~~~~~~~~~~~|6~0~~00000~~~~~~~~~~~~~|7~0~~00000~~~~~~~~~~~~~|1553294840';
 
-		assert.deepStrictEqual(btde._parse(str), {
+		assert.deepStrictEqual(btde._parse({league_key: '1BL-2020'}, str), {
 			team_names: ['TV Refrath 2', 'BC Hohenlimburg'],
+			courts: [
+				{label: '1'},
+				{label: '2'}
+			],
 			scoring: '5x11_15^90',
 			mscore: [0, 0],
 			matches: [{
@@ -109,6 +117,80 @@ describe('btde', () => {
 				name: 'HE2',
 				score: [],
 			}],
+		});
+	});
+
+	it('parse 8x 3x21 match', () => {
+		const str = '0~0~0~Nationalmannschaft~Sterkrade|1~0~H~000~~Seidel, Marvin/Lamsfuß, Mark~Benny/Steffen~3~~~2~~|2~0~~~~Roth, Fabian/Zwiebler, Marc~Commander/David~~~~~~|3~0~~~~Efler, Linda/Küspert, Stine~Leona/Jessy~~~~~~|4~0~~~~Zwiebler, Marc~Benny~~~~~~|5~0~~~~Roth, Fabian~Commander~~~~~~|6~0~~~~Seidel, Marvin~David~~~~~~|7~0~G~HG0~~Li, Yvonne~Leona~21~15~3~15~21~7|8~1~G~000~~Herttrich, Isabel/Lamsfuß, Mark~Jessy/Steffen~4~~~5~~|1601706401';
+
+		assert.deepStrictEqual(btde._parse({league_key: 'RLW-2016'}, str), {
+			team_names: ['Nationalmannschaft', 'Sterkrade'],
+			scoring: '3x21',
+			mscore: [0, 0],
+			matches: [{
+				name: 'HD1',
+				score: [[3, 2]],
+				players: [
+					[{name: 'Marvin Seidel'}, {name: 'Mark Lamsfuß'}],
+					[{name: 'Benny'}, {name: 'Steffen'}],
+				],
+				serving: 0,
+			}, {
+				name: 'HD2',
+				score: [],
+				players: [
+					[{name: 'Fabian Roth'}, {name: 'Marc Zwiebler'}],
+					[{name: 'Commander'}, {name: 'David'}],
+				],
+			}, {
+				name: 'DD',
+				score: [],
+				players: [
+					[{name: 'Linda Efler'}, {name: 'Stine Küspert'}],
+					[{name: 'Leona'}, {name: 'Jessy'}],
+				],
+			}, {
+				name: 'HE1',
+				score: [],
+				players: [
+					[{name: 'Marc Zwiebler'}],
+					[{name: 'Benny'}]
+				],
+			}, {
+				name: 'HE2',
+				score: [],
+				players: [
+					[{name: 'Fabian Roth'}],
+					[{name: 'Commander'}]
+				],
+			}, {
+				name: 'HE3',
+				score: [],
+				players: [
+					[{name: 'Marvin Seidel'}],
+					[{name: 'David'}]
+				],
+			}, {
+				name: 'DE',
+				score: [[21, 15], [15, 21], [3, 7]],
+				players: [
+					[{name: 'Yvonne Li'}],
+					[{name: 'Leona'}],
+				],
+				serving: 1,
+			}, {
+				name: 'GD',
+				score: [[4, 5]],
+				players: [
+					[{name: 'Isabel Herttrich'}, {name: 'Mark Lamsfuß'}],
+					[{name: 'Jessy'}, {name: 'Steffen'}],
+				],
+				serving: 1,
+			}],
+			courts: [
+				{label: '1', match_id: 'GD'},
+				{label: '2'}
+			],
 		});
 	});
 });
