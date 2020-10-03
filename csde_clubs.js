74c54e3bceb8a3271f032d174781e27b8962a675
Philipp Hagemeister <phihag@phihag.de>
2020-10-03 05:05:06 +0200

    csde: Update clubs and add a script to extract them

---
+++ csde_clubs.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const argparse = require('argparse');
+const fetch = require('node-fetch');
+
+const utils = require('./src/utils');
+
+
+async function main() {
+	const parser = new argparse.ArgumentParser({
+		description: 'Generate club IDs for CourtSpot, to be copied into csde.js',
+	});
+	parser.parseArgs();
+
+	for (const league of [1, 2, 3, 4]) {
+		const response = await fetch(`https://courtspot.de/php__Skripte/getVereine.php?liga=${league}`);
+		assert.equal(response.status, 200);
+		const xml = await response.text();
+
+		const [err_msg, doc] = utils.parseXML(xml);
+		assert(!err_msg, `Failed to parse XML: ${err_msg}`);
+		for (const el of Array.from(doc.getElementsByTagName('Verein'))) {
+			const name = el.getElementsByTagName('Lang')[0].textContent;
+			const id = el.getElementsByTagName('Nummer')[0].textContent;
+			console.log(`'${league}-${id}': '${name}',`);
+		}
+	}
+}
+
+
+(async () => {
+    try {
+        await main();
+    } catch (e) {
+        console.error(e.stack);
+        process.exit(2);
+    }
+})();
