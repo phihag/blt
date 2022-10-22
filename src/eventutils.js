ce4b55227f71cbb737ff9bbf9c4e7ddb9be3ac0b
Philipp Hagemeister <phihag@phihag.de>
2022-10-22 13:55:15 +0200

    unify BV Maintal

--- src/eventutils.js
+++ src/eventutils.js
@@ -159,6 +159,7 @@ function unify_team_name(team_name) {
 		'1.CfB Köln': '1. CfB Köln',
 		'TuS 1872  Schwanheim': 'TuS 1872 Schwanheim',
 		'1. BV Maintal 1978': '1. BV Maintal',
+		'1.BV Maintal': '1. BV Maintal',
 		'TSV Neuhausen-Nymphenburg': 'TSV Neuhausen-Nymphenburg München',
 		'TSV Neuhausen-Nymphenb. München': 'TSV Neuhausen-Nymphenburg München',
 		'TSV Neuhausen-Nymphenburg 2': 'TSV Neuhausen-Nymphenburg München 2',
